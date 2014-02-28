994347995f6a4df893c5c6b420c92a14e972b76f
pvfkb <patrick@boundary.com>
2014-02-28 13:24:07 +0000

    For OMD implementations
    This .mk file should be placed in the $OMD_ROOT/etc/check_mk/conf.d directory and will be applied to the managed Nagios instance once OMD is restarted.

---
+++ nagios/boundary_eventshandler.mk
@@ -0,0 +1,32 @@
+extra_nagios_conf += r"""
+
+define command {
+  command_name    handle_boundary_event_service
+  command_line    $USER2$/boundary-event-handler.rb -H $HOSTNAME$ -e service -s $SERVICESTATE$ -t $SERVICESTATETYPE$ -a $SERVICEATTEMPT$ -o "$SERVICEOUTPUT$" -d "$SERVICEDESC$"
+}
+"""
+
+extra_nagios_conf += r"""
+
+define command {
+  command_name    handle_boundary_event_host
+  command_line    $USER2$/boundary-event-handler.rb -H $HOSTADDRESS$ -e host -s $HOSTSTATE$ -t $HOSTSTATETYPE$ -a $HOSTATTEMPT$ -o $LONGHOSTOUTPUT$
+}
+"""
+
+# Define event_handler for service
+extra_service_conf["event_handler"] = [
+        ( "handle_boundary_event_service", ALL_HOSTS, ALL_SERVICES),
+]
+
+# Define event_handler_enabled for service
+extra_service_conf["event_handler_enabled"] = [
+        ( "1", ALL_HOSTS, ALL_SERVICES),
+]
+
+extra_host_conf["event_handler"] = [
+        ( "handle_boundary_event_host", ALL_HOSTS ),
+]
+extra_host_conf["event_handler_enabled"] = [
+        ( "1", ALL_HOSTS ),
+]
