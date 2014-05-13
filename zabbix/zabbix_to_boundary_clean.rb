ea2ee0ecc5f66286a95449a3599fc2d28c5cacf9
jdgwartney <davidg@boundary.com>
2014-05-13 11:50:07 -0700

    Modified query to fetch triggers to use a time constraint ; Add functionality that maps zabbix priority to boundary event severity ; Added logging using Ruby's standard logging

--- zabbix/zabbix_to_boundary_clean.rb
+++ zabbix/zabbix_to_boundary_clean.rb
@@ -21,8 +21,11 @@ require 'net/http'
 require 'net/https'
 require 'uri'
 require 'json'
-require 'httparty'
-require 'rsolr' 
+require 'time'
+require 'base64'
+require 'logger'
+
+
 
 BOUNDARY_API_HOST = "api.boundary.com"
 ZABBIX_API_HOST = "ZABBIX HOSTNAME or IP"
@@ -30,11 +33,32 @@ CACERT_PATH = "#{File.dirname(__FILE__)}/cacert.pem"
 
 # CONFIGURATIONS
 
+
 BOUNDARY_ORGID = ""
 BOUNDARY_APIKEY = ""
 ZABBIX_USER = ""
 ZABBIX_PASSWORD = ""
+ZABBIX_TIMEOUT = 30
 POLLING_PERIOD = 60
+LOGGING_LEVEL = Logger::DEBUG
+
+#
+# Controls mapping of Zabbix priority to Boundary event severity
+#
+SEVERITY_MAP = {}
+
+# Not classified
+SEVERITY_MAP[0] = "INFO"
+# Information
+SEVERITY_MAP[1] = "INFO"
+# Warning
+SEVERITY_MAP[2] = "WARN"
+# Average
+SEVERITY_MAP[3] = "ERROR"
+# High
+SEVERITY_MAP[4] = "ERROR"
+# Disaster
+SEVERITY_MAP[5] = "CRITICAL"
 
 """
 """
@@ -47,6 +71,8 @@ class BoundaryEvents
   #
 
   def initialize()
+    @logger = Logger.new(STDOUT)
+    @logger.level = LOGGING_LEVEL
     @boundary_orgid = BOUNDARY_ORGID
     @boundary_apikey = BOUNDARY_APIKEY
     @zabbix_user = ZABBIX_USER
@@ -54,10 +80,16 @@ class BoundaryEvents
     @polling_period = POLLING_PERIOD
  
     now=Time.now
+    @logger.info("Starting")
     @past= now - @polling_period
     @past_timestamp = @past.utc.iso8601     # beginning of polling period
     @past_epoch = @past.to_i
     @past_fuc = Time.at(@past).utc
+    @logger.debug("orgid=#{@boundary_orgid}, apikey=#{@boundary_apikey}")
+    @logger.debug("polling_period=#{@polling_period}")
+    @logger.debug("past: #{@past_func}")
+    @logger.debug("zabbix_user: #{@zabbix_user}")
+    @logger.debug("Using the following severity mapping: #{SEVERITY_MAP}")
   end
 
 # create a hash to login to Zabbix
@@ -83,22 +115,24 @@ class BoundaryEvents
 	:output => [ 
 		"triggerid",
 		"description",
+                "lastchange", 
 		"priority",
                 "value" 
 	],
-        :filter => {
- #	:value => 1
-        },
+      #  :filter => {
+      #	:value => 1
+      #  },
         :sortfield => "priority",
         :sortorder => "DESC",
         :expandData => true,
         :expandComment => true,
         :expandDescription => true,
- #       :lastChangeSince => 0 #  @past
+        :lastChangeSince => @past_epoch
       },
       :auth => auth_key,
       :id => 1
     }
+
     return obj
   end
 
@@ -107,6 +141,8 @@ class BoundaryEvents
     headers = { "Content-Type" => "application/json"}
     uri = URI( "http://#{ZABBIX_API_HOST}/zabbix/api_jsonrpc.php")
     http = Net::HTTP.new(uri.host, uri.port)
+
+    http.read_timeout = ZABBIX_TIMEOUT
    
     req = Net::HTTP::Post.new(uri.request_uri)
     req.body = obj.to_json
@@ -114,7 +150,7 @@ class BoundaryEvents
     headers.each{ |k,v|
       req[k] = v
     }
-
+    
     res = http.request(req)
     return zabbix_trigger_parse( res.body )
   end
@@ -137,15 +173,15 @@ class BoundaryEvents
         }
 
         res = http.request(req)       
+        @logger.debug(res)
         res_json =  JSON.parse(res.body)
-	puts res.body
         unless bad_response?(:post, uri.request_uri, res)
-          print "Obtained Zabbix Auth Key\n"
+          @logger.debug("Obtained Zabbix Auth Key")
           return res_json['result']
         end
       end
     rescue Timeout::Error
-      print "Timed out while attempting to authenticate with Zabbix"
+      @logger.error("Timed out while attempting to authenticate with Zabbix")
     end
   end
 
@@ -163,9 +199,10 @@ class BoundaryEvents
        host = subresult["host"]
        comments = subresult["comments"]
        triggerid = subresult["triggerid"]
+       priority = subresult["priority"]
  
        if lastchange_epoch > @past_epoch  #if the problem is within the polling period
-          event = parse_event( description,lastchange_ts,host,comments,triggerid,value)
+          event = parse_event(description,lastchange_ts,host,comments,triggerid,value,priority)
           events.push(event)
        end
     }
@@ -173,17 +210,29 @@ class BoundaryEvents
     return events
   end
 
+# Map the Zabbix priority(severity) to the Boundary event severity
+  def map_severity(zabbix_severity)
+    severity = SEVERITY_MAP[zabbix_severity.to_i]
+    if severity == nil
+      @logger.error("No mapping of #{zabbix_severity} to a boundary event severity")
+    end
+    return severity
+  end
+
 # parse a Zabbix trigger object into a Boundary Events API object
-  def parse_event(description,lastchange,host,comments,triggerid,value)
-    severity = "ERROR"
+  def parse_event(description,lastchange,host,comments,triggerid,value,priority)
+    severity = map_severity(priority)
     status = "OPEN"
- 
-    if value == 0
-       severity = "INFO"
+
+    # Trigger is not in a problem state, set event
+    # fields appropriately
+    if value.to_i == 0
        status = "CLOSED"
        description = "Zabbix event OK - " + description
        comments = "Zabbix reporting that Event back to OK status" 
     end
+
+    @logger.debug("[#parse_event] creating event => #{value}|#{description}|#{host}|#{status}|#{severity}|#{comments}|#{triggerid}|#{lastchange}")
         
     event = {
        :title => description,
@@ -225,14 +274,13 @@ class BoundaryEvents
         }
 
         res = http.request(req)
-
         unless bad_response?(:post, uri.request_uri, res)
-          print "Created a Boundary Event @ #{res["location"]}\n"
+          @logger.info("Created a Boundary Event @ #{res["location"]}\n")
           res["location"]
         end
       end
     rescue Timeout::Error
-      print "Timed out while attempting to create Boundary Event"
+      @logger.error("Timed out while attempting to create Boundary Event")
     end
   end
 
@@ -246,8 +294,8 @@ class BoundaryEvents
       false
     else
       true
-         print "Got a #{response.code} for #{method} to #{url}" + "\n"
-         print response.body + "\n"
+         @logger.error("Got a #{response.code} for #{method} to #{url}")
+         @logger.error(response.body)
     end
   end
 
@@ -257,12 +305,12 @@ class BoundaryEvents
     events.each do |event_body|
       create_event(event_body)
     end
+    @logger.info("Finished")
   end
-
-
 
 end
 
 
 boundary_event = BoundaryEvents.new
 boundary_event.report
+
